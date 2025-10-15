0d87e69e071704fd23e43b3dbd025bf80718caec
hugoflores <hugorilahffy@gmail.com>
2025-10-15 08:54:55 -0400

    Clase 15 de Octubre

--- app/src/main/java/com/prograiii/appdeclases/EjemploScrollActivity.kt
+++ app/src/main/java/com/prograiii/appdeclases/EjemploScrollActivity.kt
@@ -1,12 +1,22 @@
 package com.prograiii.appdeclases
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class EjemploScrollActivity : AppCompatActivity() {
+
+    val context: Context = this
+
+    companion object {
+        val ID_HOLA_MUNDO = "id_hola_mundo"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,15 @@ class EjemploScrollActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button: Button = findViewById(R.id.button_ejemplo_scroll)
+        button.setOnClickListener {
+            val intentCambioPantalla: Intent = Intent(context , MainActivity::class.java)
+            intentCambioPantalla.apply {
+                putExtra(ID_HOLA_MUNDO,"Hola Mundo Enviado")
+            }
+            startActivity(intentCambioPantalla)
         }
     }
 }
--- app/src/main/java/com/prograiii/appdeclases/MainActivity.kt
+++ app/src/main/java/com/prograiii/appdeclases/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.prograiii.appdeclases
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.prograiii.appdeclases.EjemploScrollActivity.Companion.ID_HOLA_MUNDO
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textView: TextView = findViewById(R.id.text_view_ejemplo6)
+        val stringReci: String? = intent.getStringExtra(ID_HOLA_MUNDO)
+        textView.text = stringReci
     }
 }
